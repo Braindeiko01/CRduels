6498f8436b57c4aaebeb7b1d6a90e77f41064d53
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-06-21 23:31:12 -0500

    Add SSE support for matchmaking

--- CrDuels/src/main/java/com/crduels/application/controller/SseController.java
+++ CrDuels/src/main/java/com/crduels/application/controller/SseController.java
@@ -1,8 +1,10 @@
 package com.crduels.application.controller;
 
 import com.crduels.application.service.SseService;
+import com.crduels.application.service.MatchSseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class SseController {
 
     private final SseService sseService;
+    private final MatchSseService matchSseService;
 
     @GetMapping("/transacciones")
     public SseEmitter streamTransacciones() {
         return sseService.subscribe();
+    }
+
+    @GetMapping("/match")
+    public SseEmitter streamMatch(@RequestParam("jugadorId") String jugadorId) {
+        return matchSseService.subscribe(jugadorId);
     }
 }
---
+++ CrDuels/src/main/java/com/crduels/infrastructure/dto/rs/MatchSseDto.java
@@ -0,0 +1,23 @@
+package com.crduels.infrastructure.dto.rs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MatchSseDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -5076615234782375916L;
+
+    private UUID apuestaId;
+    private String jugadorOponenteId;
+    private String jugadorOponenteTag;
+}
