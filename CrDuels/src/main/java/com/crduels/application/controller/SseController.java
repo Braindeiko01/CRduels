6673dbe3451014a64d68166d5b1825dd10b45741
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-06-23 19:46:36 -0500

    Support legacy SSE route

--- CrDuels/src/main/java/com/crduels/application/controller/SseController.java
+++ CrDuels/src/main/java/com/crduels/application/controller/SseController.java
@@ -4,6 +4,7 @@ import com.crduels.application.service.SseService;
 import com.crduels.application.service.MatchSseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -22,8 +23,13 @@ public class SseController {
         return sseService.subscribe();
     }
 
+    @GetMapping("/matchmaking/{usuarioId}")
+    public SseEmitter streamMatch(@PathVariable("usuarioId") String usuarioId) {
+        return matchSseService.subscribe(usuarioId);
+    }
+
     @GetMapping("/match")
-    public SseEmitter streamMatch(@RequestParam("jugadorId") String jugadorId) {
+    public SseEmitter streamMatchLegacy(@RequestParam("jugadorId") String jugadorId) {
         return matchSseService.subscribe(jugadorId);
     }
 }
